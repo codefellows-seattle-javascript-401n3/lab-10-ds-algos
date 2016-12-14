a825cb73893448ac08e80f8d5c0502d91cfd461b
Trenton Kress <trenton.kress@gmail.com>
2016-12-14 03:31:41 -0800

    Set up the variables for the Hanoi problem.

--- lab-trent/index.js
+++ lab-trent/index.js
@@ -2,4 +2,10 @@
 
 const Stack = require('./lib/stack');
 
-let s1 = new Stack();
+let a = new Stack([6, 5, 4, 3, 2, 1]);
+let b = new Stack();
+let c = new Stack();
+
+function solveHanoi(a, b, c) {
+  
+}
