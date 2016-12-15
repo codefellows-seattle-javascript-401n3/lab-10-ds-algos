a665a552fa62906aa991639d1d5967695d2fd4a4
Trenton Kress <trenton.kress@gmail.com>
2016-12-14 17:40:29 -0800

    Added tests for the hanoi iterative solution.

--- lab-trent/test/data-structures-test.js
+++ lab-trent/test/data-structures-test.js
@@ -3,6 +3,7 @@
 const expect = require('chai').expect;
 const Stack = require('../lib/stack');
 const Queue = require('../lib/queue');
+const hanoi = require('../lib/hanoisolutions');
 
 describe('Data Structures', function() {
   describe('Stack', function() {
@@ -88,6 +89,53 @@ describe('Data Structures', function() {
       it('should return and remove the value from the head of the queue', function() {
         let q = new Queue([2, 3, 6, 12]);
         expect(q.dequeue()).to.equal(2);
+      });
+    });
+  });
+
+  describe('HanoiSolutions', function() {
+    describe('#solveHanoiIterative()', function() {
+      it('should be able to solve a 3 ring tower', function() {
+        let a = new Stack([3, 2, 1]);
+        let b = new Stack();
+        let c = new Stack();
+
+        hanoi.solveHanoiIterative(a, b, c);
+
+        expect(c.pop()).to.equal(1);
+        expect(c.pop()).to.equal(2);
+        expect(c.pop()).to.equal(3);
+      });
+      it('should be able to solve a 6 ring tower', function() {
+        let a = new Stack([6, 5, 4, 3, 2, 1]);
+        let b = new Stack();
+        let c = new Stack();
+
+        hanoi.solveHanoiIterative(a, b, c);
+
+        expect(c.pop()).to.equal(1);
+        expect(c.pop()).to.equal(2);
+        expect(c.pop()).to.equal(3);
+        expect(c.pop()).to.equal(4);
+        expect(c.pop()).to.equal(5);
+        expect(c.pop()).to.equal(6);
+      });
+      it('should be able to solve a 9 ring tower', function() {
+        let a = new Stack([9, 8, 7, 6, 5, 4, 3, 2, 1]);
+        let b = new Stack();
+        let c = new Stack();
+
+        hanoi.solveHanoiIterative(a, b, c);
+
+        expect(c.pop()).to.equal(1);
+        expect(c.pop()).to.equal(2);
+        expect(c.pop()).to.equal(3);
+        expect(c.pop()).to.equal(4);
+        expect(c.pop()).to.equal(5);
+        expect(c.pop()).to.equal(6);
+        expect(c.pop()).to.equal(7);
+        expect(c.pop()).to.equal(8);
+        expect(c.pop()).to.equal(9);
       });
     });
   });
