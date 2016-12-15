3ccafd9c0fde7db8258d27c5096af89761eb4fd2
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-12-14 22:30:16 -0800

    Finished stack tests. All passing

--- lab-jessica/lib/stack.js
+++ lab-jessica/lib/stack.js
@@ -7,16 +7,27 @@ const Stack = module.exports = function(maxSize) {
 };
 
 Stack.prototype.push = function(val) {
+  if(this.maxSize && this.size === this.maxSize) {
+    return Error;
+  }
   this.top = new Node(val, this.top);
   this.size += 1;
 };
 
 Stack.prototype.pop = function() {
-  const currentTop = this.top;
-  this.top = currentTop.prev;
-  return currentTop;
+  if(this.top) {
+    const currentTop = this.top;
+    this.top = currentTop.prev;
+    this.size --;
+    return currentTop.val;
+  }
+  return null;
 };
 
 Stack.prototype.peek = function() {
+  if(this.top) {
+    return this.top.val;
+  }
+
   return this.top;
 };
