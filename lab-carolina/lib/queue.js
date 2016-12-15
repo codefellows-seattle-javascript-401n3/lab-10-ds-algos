f81bd3b413c8bf2e6d0f0b0a9b897fd6d1dcc7fa
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-12-14 20:31:49 -0800

    stack tests passing

---
+++ lab-carolina/lib/queue.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Node = require('../node.js');
+
+const Queue = module.exports = function(){
+  this.front = null;
+  this.back = null;
+};
+
+Queue.prototye.enqueue = function(){
+//create a new node using val as node.val
+//aand assign the back.prev to the new node
+//assign the new node to the back of the Q
+  const newNode = new Node(val);
+  if(!this.back){
+    this.back = newNode;
+    this.front = this.back;
+    return; //to break here and run next block of code, or wrap next block of code in an else block
+  }
+  this.back.prev = newNode;
+  this.back = newNode;
+};
+
+Queue.prototype.dequeue = function(){
+  if(!this.front) throw new Error(error);
+  let temp = this.front.val;
+  if(this.front === this.back){
+    this.back = null;
+  }
+  this.front = this.front.prev;
+  return temp;
+};
