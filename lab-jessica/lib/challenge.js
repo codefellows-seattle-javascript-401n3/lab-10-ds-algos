58c30165ec27239c6a3bc4d6598c3bd6b77fc0a3
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-12-16 22:11:32 -0800

    Almost working, needs to be cleaned up

--- lab-jessica/lib/challenge.js
+++ lab-jessica/lib/challenge.js
@@ -1,51 +1,92 @@
 'use strict';
-console.log('code is running');
 
 // app modules
-// const Node = require('./node.js');
 const Stack = require('./stack.js');
 
 let stackA = new Stack();
 let stackB = new Stack();
 let stackC = new Stack();
 
+//do these indices point to the same value in memory as objects above??
 let stacks = [stackA, stackB, stackC];
-
-// need to create stack a and push n disks on to it
+// this is where we start
+let counter = 0;
+//this is the starting position of the smallest disk
+let currentIndex = 0;
+// need to create stack a and push n numbers on in correct order
 function createTowerA(n) {
   for(let i = n ; i > 0 ; i--) {
     stackA.push(i);
   }
 }
 
-//this should move a disk top
-
-// we want to identify the stack with the smallest top
-function identifySmallestTop() {
-  let smallestTop = stacks[0].top.val;
-  // looping through stacks to find smallest disk
-  for(let i = 0; i < stacks.length ; i++) {
-    if(stacks[i] && stacks[i].top.val < smallestTop) {
-      smallestTop = stacks[i].top.val;
-    }
+// we should keep track of smallest disk's current position
+// always move +1 index
+// if index is index 2 then move to index 0
+function moveSmallestDisk() {
+// we dont need to save this val because the smallest will always be 1
+  stacks[currentIndex].pop();
+  if(currentIndex !== 2) {
+    stacks[currentIndex + 1].push(1);
+// smallest disk has new location so we need to reassign currentIndex
+    currentIndex ++;
+    return;
   }
-  console.log(smallestTop);
-  return smallestTop;
+  stacks[0].push(1);
+// smallest disk has new location so we need to reassign currentIndex
+  currentIndex = 0;
+}
+// compare top of stacks not containing smallestTop
+// the smaller of the two will be moved to the top of the other
+// what about null tops?
+// if one of the stacks is null we will move to that stack
+function nextSmallestDisk() {
+  const compArr = [{stack: stacks[0], index: 0}, {stack: stacks[1], index: 1}, {stack: stacks[2], index: 2}];
+  compArr.splice(currentIndex, 1);
+// stacks.splice(currentIndex, 1); //this was changing original stacks arr
+  if(compArr[0].stack.top === null) {
+    const smallerVal = stacks[compArr[1].index].pop();
+    console.log('val popped', smallerVal);
+    stacks[compArr[0].index].push(smallerVal);
+    console.log(stacks);
+    return;
+  } else if(compArr[1].stack.top === null) {
+    const smallerVal = stacks[compArr[0].index].pop();
+    console.log('val popped', smallerVal);
+    stacks[compArr[1].index].push(smallerVal);
+    console.log(stacks);
+    return;
+  } else if(compArr[0].stack.top.val < compArr[1].stack.top.val ) {
+    const smallerVal = stacks[compArr[0].index].pop();
+    console.log('val popped', smallerVal);
+    stacks[compArr[1].index].push(smallerVal);
+    console.log(stacks);
+    // console.log(stacks[2].top.prev);
+    return;
+  } else {
+    const smallerVal = stacks[compArr[1].index].pop();
+    console.log('val popped', smallerVal);
+    stacks[compArr[0].index].push(smallerVal);
+    console.log(stacks);
+  }
 }
 
-stackB.push(6);//just testing identifySmallestTop
-stackC.push(4);//just testing identifySmallestTop
+function runChallenge(numDisks) {
 
+  createTowerA(numDisks);
 
-//this would go in index.js
-createTowerA(6);
-stackA.push(24); //just testing identifySmallestTop
-identifySmallestTop();
+  while (stacks[1].size !== numDisks || stacks[2].size !== numDisks) {
+    console.log(counter);
+    if(counter % 2 === 0) {
+      moveSmallestDisk();
+      counter ++;
+    } else {
+      nextSmallestDisk();
+      counter ++;
+    }
+  }
+  console.log('final stacks', stacks);
+  console.log('final counter', counter);
+}
 
-
-// 2. Complete the code challenge using either an iterative or recursive solution
-//   * If you choose to do both, put them in separate modules and document them in your README
-//
-//
-// * The objective of the puzzle is to move the entire stack of disks from the starting stack to a new stack (i.e. from A to C, or A to B), obeying the following rules:
-//   * No disk may be placed on top of a smaller disk
+runChallenge(3);
