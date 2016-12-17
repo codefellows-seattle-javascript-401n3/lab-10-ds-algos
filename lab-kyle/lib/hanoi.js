649ca5b445ecdda0abc9e4a45303e830002d1db6
Kyle Winckler <kyle.winckler@gmail.com>
2016-12-17 15:06:58 -0800

    implemented hanoi solution

--- lab-kyle/lib/hanoi.js
+++ lab-kyle/lib/hanoi.js
@@ -1,16 +1,80 @@
 'use strict'
 
-const Node = require('./node')
 const Stack = require('./stack')
 
 // create 3 towers
 let a = new Stack()
 let b = new Stack()
-let c = new Stack(); // had to add a semi colon to remove linter bug error
+let c = new Stack()
 
-// intialize 6 discs (nodes) to tower a
-[6, 5, 4, 3, 2, 1].forEach(ele => {
-  a.push(ele)
-})
-// 2^n-1
-console.log(a);
+let turns = 0
+
+function hanoi(n) {
+  initStackA(n)
+  if (typeof n === 'number') {
+    while (b.size < n && c.size < n) {
+      if (n % 2 === 0) {
+        ab()
+        ac()
+        bc()
+      } else {
+        ac()
+        ab()
+        bc()
+      }
+    }
+    console.log(turns)
+    console.log(a.size, b.size, c.size)
+  }
+  return new Error('Please pass in a number')
+}
+
+function initStackA(n) {
+  while(n) {
+    a.push(n)
+    n --
+  }
+}
+// function to handle moves between stack a and stack b
+function ab() {
+  if (!b.top) {
+    b.push(a.pop())
+  } else if (!a.top) {
+    a.push(b.pop())
+  } else if (a.top.val < b.top.val) {
+    b.push(a.pop())
+  } else {
+    a.push(b.pop())
+  }
+  turns ++
+}
+
+// function to handle moves between stack a and stack c
+function ac() {
+  if (!c.top) {
+    c.push(a.pop())
+  } else if (!a.top) {
+    a.push(c.pop())
+  } else if (a.top.val < c.top.val) {
+    c.push(a.pop())
+  } else {
+    a.push(c.pop())
+  }
+  turns ++
+}
+
+// function to handle moves between stack b and stack c
+function bc() {
+  if (!c.top) {
+    c.push(b.pop())
+  } else if (!b.top) {
+    b.push(c.pop())
+  } else if (b.top.val < c.top.val) {
+    c.push(b.pop())
+  } else {
+    b.push(c.pop())
+  }
+  turns ++
+}
+
+hanoi(6)
